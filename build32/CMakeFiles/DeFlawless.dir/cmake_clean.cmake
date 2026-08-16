ab5411d6dc6a945601204a526e66b1141c87b569
XaToR <43992639+xatornet@users.noreply.github.com>
2026-08-16 18:53:57 +0200

    Add files via upload

---
+++ build32/CMakeFiles/DeFlawless.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/DeFlawless.dir/src/DeFlawless.cpp.obj"
+  "CMakeFiles/DeFlawless.dir/src/DeFlawless.cpp.obj.d"
+  "DeFlawless.asi"
+  "DeFlawless.asi.manifest"
+  "DeFlawless.pdb"
+  "libDeFlawless.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/DeFlawless.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
